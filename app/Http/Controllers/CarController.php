50e9d06c355c1f6dba0481a6d204aae1365bf03e
Kevin Espinoza <kevinespinoza1263@gmail.com>
2021-06-04 01:03:09 -0300

    CRUD of Cars complete with JWT
    Methods PUT, DELETE, POST, GET.

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -74,4 +74,84 @@ class CarController extends Controller
 
         return response()->json($data, 200);
     }
+
+    public function show($id){
+        $car = Car::find($id)->load('user');
+        return response()->json([
+            'car' => $car,
+            'status' => 'success'
+        ], 200);
+    }
+
+    public function update($id, Request $request){
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if ($checkToken) {
+            // Get data from POST
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true);
+
+            //Data validation
+            $validate = \Validator::make($params_array, [
+                'title' => 'required | min:5',
+                'description' => 'required',
+                'price' => 'required',
+                'status' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                return response()->json($validate->errors(), 400);
+            }
+
+            //Update car
+            $car = Car::where('id', $id)->update($params_array);
+            $data = [
+                'car' => $params,
+                'status' => 'success',
+                'code' => 200
+            ];
+
+
+        }else{
+            // Return error
+            $data = [
+                'message' => 'Login incorrecto.',
+                'status' => 'error',
+                'code' => 400
+            ];
+        }
+    }
+
+    public function destroy($id, Request $request){
+        $hash = $request->header('Authorization', null);
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if ($checkToken) {
+            // Check if register exist
+            $car = new Car::find($id);
+
+            //Delete register
+            $car->delete();
+
+            // Return deleted register
+            $data = [
+                'car' => $car,
+                'status' => 'success',
+                'code' => 200
+            ];
+        }else{
+            $data = [
+                'status' => 'error',
+                'message' => 'Login Incorrecto.',
+                'code' => 400
+            ];
+        }
+
+        return response()->json($data, 200);
+    }
 }
